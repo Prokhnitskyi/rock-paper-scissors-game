2fde71b1b0e75d9ba8d1530bb284c542ec6bb9d3
andrii.prokhnitskyi <26540483+Prokhnitskyi@users.noreply.github.com>
2023-04-16 14:13:30 +0300

    Add function for playRound

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
+const possibleValues = ['rock', 'paper', 'scissors'];
+
 function getComputerChoice() {
-    const results = ['rock', 'paper', 'scissors'];
     const randomIndex = Math.floor(Math.random() * 3);
-    return results.at(randomIndex);
+    return possibleValues.at(randomIndex);
+}
+
+function playRound(playerSelection, computerSelection) {
+    const winLoseConditions = {
+        rock: {
+            paper: 'lose',
+            scissors: 'win'
+        },
+        paper: {
+            rock: 'win',
+            scissors: 'lose'
+        },
+        scissors: {
+            rock: 'lose',
+            paper: 'win'
+        }
+    }
+    const playerSelectionClean =  playerSelection.toLowerCase().trim();
+
+    if(!possibleValues.includes(playerSelectionClean)) {
+        throw new Error(`You should select ${possibleValues.join(' or ')}`);
+    }
+
+    if (playerSelectionClean === computerSelection) {
+        return 'tie';
+    } else {
+        return winLoseConditions[playerSelectionClean][computerSelection];
+    }
 }
